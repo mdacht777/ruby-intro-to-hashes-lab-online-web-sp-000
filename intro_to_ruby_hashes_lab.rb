cbf20515f69402ff89c2e393dce62f50ae202525
Martin Achtermann <mdacht777@gmail.com>
2020-06-13 23:50:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,13 +30,16 @@ def monopoly_with_third_tier
       },
       pennsylvania_railroad:
       {
-      },
+        "mortgage_value"=>"$200"
+
       b_and_o_railroad:
       {
-      },
+        "mortgage_value"=>"$400"
+
       shortline:
       {
-      }
+        "mortgage_value"=>"$800"
+
     },
 	  rent_in_dollars:{
 	    one_piece_owned:25,
